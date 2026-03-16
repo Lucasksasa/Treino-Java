0f006e52df85636a03bbe5039daa73ee9f0d09e3
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-03-16 17:37:22 -0300

    feat: implementar sistema para pedir a idade correta do usuário

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+
+        String nome = "Lucas";
+
+
+        System.out.println("Digite sua idade, " + nome + ": ");
+        int idade = scanner.nextInt();
+
+        if (idade > 25)
+            System.out.println("Essa idade não é a verdadeira, tu sabe");
+
+        else if (idade < 25) {
+            System.out.println("Essa idade não é verdadeira, chará");
+
         }
+
+
+        while (idade != 25){
+            System.out.println("tente novamente, digite sua idade: ");
+            idade = scanner.nextInt();
+        }
+
+        System.out.println("AAAhhh, agora sim");
+
     }
 }
